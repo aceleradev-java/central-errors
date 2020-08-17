87a16b5a5bcd70d818361854cd9c779f2cf198eb
Adriano Avelino <adriano-st777@hotmail.com>
2020-08-17 20:41:24 -0300

    inicia controle de versão

---
+++ src/main/java/br/com/aceleradev/centralerros/CentralErrosApplication.java
@@ -0,0 +1,13 @@
+package br.com.aceleradev.centralerros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralErrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralErrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/aceleradev/centralerros/CentralErrosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aceleradev.centralerros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralErrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
