a080f1355e88a6fbc591810473502e3118a60dcf
Adriano Avelino <adriano-st777@hotmail.com>
2020-08-27 22:31:35 -0300

    adiciona a notação de repository

--- src/main/java/br/com/aceleradev/centralerrors/repository/UserRepository.java
+++ src/main/java/br/com/aceleradev/centralerrors/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package br.com.aceleradev.centralerrors.repository;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.aceleradev.centralerrors.entity.User;
 
+@Repository
 public interface UserRepository extends PagingAndSortingRepository<User, Long> {
     User findByUsername(String username);
 }
