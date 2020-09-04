fd5796ad6741cba4c1f7b607f85b71bfecb46b53
Adriano Avelino <adriano-st777@hotmail.com>
2020-09-03 23:31:37 -0300

    adiciona exception para nova senha e confirmação não coincidem

--- src/main/java/br/com/aceleradev/centralerrors/handle/ApiExceptionHandler.java
+++ src/main/java/br/com/aceleradev/centralerrors/handle/ApiExceptionHandler.java
@@ -25,6 +25,7 @@ import br.com.aceleradev.centralerrors.error.Problem;
 import br.com.aceleradev.centralerrors.error.Problem.Field;
 import br.com.aceleradev.centralerrors.exception.ActionNotAllowed;
 import br.com.aceleradev.centralerrors.exception.EntityNotFound;
+import br.com.aceleradev.centralerrors.exception.PasswordNotMatchException;
 import br.com.aceleradev.centralerrors.exception.UsernameAlreadyExists;
 
 @RestControllerAdvice
@@ -51,6 +52,14 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     @ResponseBody
     public Problem handleEntityNotFound(ActionNotAllowed ex, WebRequest request) {
         HttpStatus status = HttpStatus.FORBIDDEN;
+        return new Problem(status.value(), LocalDateTime.now(), ex.getMessage(), null, new ArrayList<Field>());
+    }
+    
+    @ExceptionHandler(PasswordNotMatchException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public Problem handleEntityNotFound(PasswordNotMatchException ex, WebRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         return new Problem(status.value(), LocalDateTime.now(), ex.getMessage(), null, new ArrayList<Field>());
     }
     
