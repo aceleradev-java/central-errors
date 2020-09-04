a4b9d81ed2b0465f9125535c586b00653f7770f2
Adriano Avelino <adriano-st777@hotmail.com>
2020-09-03 23:47:05 -0300

    atualiza anotação de notnull

--- src/main/java/br/com/aceleradev/centralerrors/entity/User.java
+++ src/main/java/br/com/aceleradev/centralerrors/entity/User.java
@@ -7,8 +7,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
-
-import com.sun.istack.NotNull;
+import javax.validation.constraints.NotNull;
 
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
