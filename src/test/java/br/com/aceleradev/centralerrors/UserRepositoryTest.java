3a77438ea21be316393925c1cdc124c5b14ffcd3
Adriano Avelino <adriano-st777@hotmail.com>
2020-11-10 20:33:06 -0300

    adiciona testes de repository para User
    Os teste adicionados foram para deletar, atualizar, buscar por id e 
    buscar por nome de um usuário

--- src/test/java/br/com/aceleradev/centralerrors/UserRepositoryTest.java
+++ src/test/java/br/com/aceleradev/centralerrors/UserRepositoryTest.java
@@ -3,9 +3,10 @@ package br.com.aceleradev.centralerrors;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import br.com.aceleradev.centralerrors.entity.User;
 import br.com.aceleradev.centralerrors.repository.UserRepository;
@@ -19,11 +20,63 @@ class UserRepositoryTest {
 	@Test
 	void shouldPersisteNewValidUser() {
 		User user = new User("adriano", "123", "Adriano Avelino", false);
+		
 		this.repository.save(user);
+		
 		assertThat(user.getUsername(), is("adriano"));
 		assertThat(user.getName(), is("Adriano Avelino"));
 		assertThat(user.getPassword(), is("123"));
 		assertThat(user.isAdmin(), is(false));
+	}
+	
+	@Test
+	void shouldDeleteAnUser() {
+		User user = new User("adriano", "123", "Adriano dos Santos", true);
+		this.repository.save(user);
+		assertThat(3l, is(this.repository.count()));
+		
+		this.repository.delete(user);
+		
+		assertThat(2l, is(this.repository.count()));
+	}
+	
+	@Test
+	void shouldUpdateAnUser() {
+		User user = new User("adriano", "123", "Adriano dos Santos", true);
+		this.repository.save(user);
+		
+		user.setUsername("usuário alterado");
+		user.setPassword("1234");
+		user.setName("nome alterado");
+		user.setAdmin(false);
+		this.repository.save(user);
+		
+		assertThat("usuário alterado", is(user.getUsername()));
+		assertThat("1234", is(user.getPassword()));
+		assertThat("nome alterado", is(user.getName()));
+		assertThat(false, is(user.isAdmin()));
+	}
+	
+	@Test
+	void shouldFindUserById() {
+		User user = new User("adriano", "123", "Adriano dos Santos", true);
+		this.repository.save(user);
+		
+		User userFound = this.repository.findById(user.getId()).orElse(null);
+		
+		Assertions.assertNotNull(userFound);
+		assertThat("adriano", is(userFound.getUsername()));
+	}
+	
+	@Test
+	void shouldFinduserByUsername() {
+		User user = new User("adriano", "123", "Adriano dos Santos", true);
+		this.repository.save(user);
+		
+		User userFound = this.repository.findByUsername("adriano");
+		
+		Assertions.assertNotNull(userFound);
+		assertThat("adriano", is(userFound.getUsername()));
 	}
 
 }
