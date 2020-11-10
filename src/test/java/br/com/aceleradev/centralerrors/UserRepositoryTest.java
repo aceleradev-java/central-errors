118d89a47c98f828305e8a92fa24108ddf644deb
Adriano Avelino <adriano-st777@hotmail.com>
2020-11-09 22:26:21 -0300

    adiciona teste para cadastro de usuário com dados válidos

---
+++ src/test/java/br/com/aceleradev/centralerrors/UserRepositoryTest.java
@@ -0,0 +1,29 @@
+package br.com.aceleradev.centralerrors;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import br.com.aceleradev.centralerrors.entity.User;
+import br.com.aceleradev.centralerrors.repository.UserRepository;
+
+@SpringBootTest
+class UserRepositoryTest {
+
+	@Autowired
+	private UserRepository repository;
+
+	@Test
+	void shouldPersisteNewValidUser() {
+		User user = new User("adriano", "123", "Adriano Avelino", false);
+		this.repository.save(user);
+		assertThat(user.getUsername(), is("adriano"));
+		assertThat(user.getName(), is("Adriano Avelino"));
+		assertThat(user.getPassword(), is("123"));
+		assertThat(user.isAdmin(), is(false));
+	}
+
+}
