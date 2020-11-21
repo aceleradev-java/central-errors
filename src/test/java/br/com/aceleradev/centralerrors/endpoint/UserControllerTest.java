a8552bcdcdaa2a43134115f0d0ae32529d590725
Adriano Avelino <adriano-st777@hotmail.com>
2020-11-20 22:13:52 -0300

    extrai headers de autenticação dos testes de UserControllerTest

--- src/test/java/br/com/aceleradev/centralerrors/endpoint/UserControllerTest.java
+++ src/test/java/br/com/aceleradev/centralerrors/endpoint/UserControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -25,22 +26,28 @@ class UserControllerTest {
 	@Autowired
 	private UserService service;
 
+	
+	private HttpHeaders getAdminHeaders() throws Exception {
+    	String authenticationCredential = "{\"username\":\"admin\",\"password\":\"123\"}";
+		MvcResult authenticationResult = mockMvc.perform(MockMvcRequestBuilders.post("/login")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(authenticationCredential))
+				.andReturn();
+		String authorizationValue = authenticationResult.getResponse().getContentAsString();
+    	HttpHeaders adminHeader = new HttpHeaders();
+		adminHeader.setBearerAuth(authorizationValue);
+    	return adminHeader;
+    }
+    
 	@Test
 	void findById() throws Exception {
 		User user = new User("adriano", "123", "Adriano dos Santos", true);
 		this.service.save(user);
-		
-		String str = "{\"username\":\"admin\",\"password\":\"123\"}";
-		MvcResult authenticationresult = mockMvc.perform(MockMvcRequestBuilders.post("/login")
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(str))
-				.andReturn();
-		String token = authenticationresult.getResponse().getContentAsString();
 		String id = user.getId().toString();
 		
 		MvcResult result = mockMvc
 				.perform(MockMvcRequestBuilders.get("/v1/admin/users/{id}", id)
-						.header("Authorization", token))
+						.headers(this.getAdminHeaders()))
 				.andDo(MockMvcResultHandlers.print())
 				.andExpect(MockMvcResultMatchers.status().isOk())
 				.andReturn();
